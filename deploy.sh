43d72e5753251c064d2a52fdf5d0ea3407078b36
Doug Richardson <dougie.richardson@gmail.com>
2015-12-14 12:57:36 -0800

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo "Deploying..."
+echo "Make sure your google compute SSH identity is added. If not, use:"
+echo "   ssh-add ~/.ssh/google_compute_engine" 
+
+cd site
+jekyll build
+rsync -av _site dougrichardson.org:/home/doug
