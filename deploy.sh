06b979eda5ce30519a94b734d4a4a318b152094d
Doug Richardson <doug@rekt.email>
2018-08-10 22:30:02 -0700

    update with key on pc

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -xeuo pipefail
 echo "Deploying..."
 echo "Make sure your google compute SSH identity is added. If not, use:"
 echo "   ssh-add ~/.ssh/google_compute_engine" 
+echo "   ssh-add ~/.ssh/google_compute_engine_PERSONAL"
 
 cd site
 jekyll build
