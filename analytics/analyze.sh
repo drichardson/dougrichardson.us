432232dd8f232232b19c70ac53f890124b806009
Doug Richardson <doug@rekt.email>
2018-09-17 15:52:05 -0700

    write analysis to file

--- analytics/analyze.sh
+++ analytics/analyze.sh
@@ -2,4 +2,5 @@
 
 unzippedLogs=$(ls -1 logs/access.log*|grep -v ".gz")
 
-zcat logs/access.log*.gz | cat - $unzippedLogs | goaccess -a --log-format '%h %^[%d:%t %^] "%r" %s %b' --date-format '%d/%b/%Y'  --time-format '%H:%M:%S'
+zcat logs/access.log*.gz | cat - $unzippedLogs | goaccess -a --log-format '%h %^[%d:%t %^] "%r" %s %b' --date-format '%d/%b/%Y'  --time-format '%H:%M:%S' -o report.html --log-format=COMBINED
+
