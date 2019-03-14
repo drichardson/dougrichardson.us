e413c6a444303b53e0b7e035b8cc810478f7aa23
Doug Richardson <doug@rekt.email>
2019-03-14 16:05:40 -0700

    exclude c standard from link check since its working but timing out.

--- deadlink_check.sh
+++ deadlink_check.sh
@@ -7,6 +7,9 @@ shift
 
 echo "Checking links for $URL"
 
-muffet "$URL" --verbose --exclude "linkedin.com/in/douglasrichardson" -e "stackexchange.com/a/"
+muffet "$URL" --verbose \
+  -e "linkedin.com/in/douglasrichardson" \
+  -e "stackexchange.com/a/" \
+  -e "http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1256.pdf"
 
 echo "All links okay"
