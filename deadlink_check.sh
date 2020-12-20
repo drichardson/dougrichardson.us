f46cdc03187384c5f444fc67f3ed63bd343f1c82
Doug Richardson <git@rekt.email>
2020-12-20 12:51:56 -0800

    Use a firefox user agent so user agent sniffing sites don't reject you

--- deadlink_check.sh
+++ deadlink_check.sh
@@ -5,14 +5,25 @@ set -uoe pipefail
 URL=$1
 shift
 
-go get github.com/raviqqe/muffet
+which muffet > /dev/null
+if [[ $? -ne 0 ]]; then
+	echo muffet not installed. Run:
+	echo   go get github.com/raviqqe/muffet
+	echo Also make sure muffet is in your PATH.
+	exit 1
+fi
 
 echo "Checking links for $URL"
 
-~/go/bin/muffet "$URL" --verbose \
-  -e "linkedin.com/in/douglasrichardson" \
-  -e "stackexchange.com/a/" \
-  -e "http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1256.pdf" \
-  -e "https://github.com/EpicGames/UnrealEngine"
+# Use an actual browser user agent to get around browser sniffing issues.
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
+
+muffet \
+	--header="User-Agent: $USER_AGENT" \
+	--verbose \
+	-e "http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1256.pdf" \
+	-e "https://github.com/EpicGames/UnrealEngine" \
+	-e "https://www.linkedin.com" \
+	"$URL"
 
 echo "All links okay"
