597abc5e021fecfdcd6601fd692fc6da9b18adb6
Doug Richardson <git@rekt.email>
2021-06-14 12:41:05 -0700

    include license

--- fetch_data.py
+++ fetch_data.py
@@ -7,18 +7,28 @@ import time
 import yaml
 
 
-def make_ueplugin_entry(name, description, url):
-    return {
-            'name': name,
-            'description' : description,
-            'url' : url
-            }
+def make_ueplugin_entry(name, description, url, license_name, license_url):
+    entry = {
+        'name': name,
+        'description': description,
+        'url': url,
+    }
+
+    if license_name:
+        license = { 'name' : license_name }
+        if license_url:
+            license['url'] = license_url
+        entry['license'] = license
+
+    return entry
+
 
 def required_env(name):
     val = os.environ[name]
     if val is None:
         raise Exception(f'Environment variable {name} is not set')
     return val
+
 
 def update_ueplugins():
     UEPLUGINS_INPUT = 'data.yml'
@@ -36,15 +46,27 @@ def update_ueplugins():
     for repo_name in data['ueplugins']['github']:
         print(f'Fetching github repo {repo_name}')
         repo = gh.get_repo(repo_name)
-        entry = make_ueplugin_entry(repo.name, repo.description, repo.html_url)
+        try:
+            lic = repo.get_license().license
+            lic_name = lic.spdx_id
+            lic_url = lic.html_url
+        except github.GithubException:
+            print(f'Could not find license for github repo {repo_name}')
+            lic_name = None
+            lic_url = None
+
+        entry = make_ueplugin_entry(
+            repo.name, repo.description, repo.html_url, lic_name, lic_url)
         ueplugins.append(entry)
 
     print('Processing gitlab repos')
     gl = gitlab.Gitlab('https://gitlab.com', private_token=GITLAB_TOKEN)
     for repo_id in data['ueplugins']['gitlab']:
         print(f'Fetching gitlab repo {repo_id}')
-        p = gl.projects.get(repo_id)
-        entry = make_ueplugin_entry(p.name, p.description, p.web_url)
+        p=gl.projects.get(repo_id, license=True)
+        lic=p.license
+        entry=make_ueplugin_entry(p.name, p.description, p.web_url,
+            lic['key'], lic['html_url'])
         ueplugins.append(entry)
 
     print(f'Writing to {UEPLUGINS_OUTPUT}')
--- site/_notes/ueplugins.md
+++ site/_notes/ueplugins.md
@@ -1,18 +1,20 @@
 ---
 layout: note
-title: "Unreal Engine Plugins"
+title: "Open Source Unreal Engine Plugins"
 mathjax: true
 date: 2021-06-13 11:51:05 -0700
 ---
 
 A list of open source Unreal Engine Plugins.
 
-| Name | Description | 
-|------|-------------|
+Some of these plugins can also be purchased on the UE Marketplace.
+
+If you think a plugin should be added to the list, please let me know.
+
+| Name | Description | License |
+|------|-------------|---------|
 {% assign plugins = site.data.ueplugins | sort_natural: "name" -%}
 {% for plugin in plugins -%}
-| [{{ plugin.name }}]({{ plugin.url }}) | {{ plugin.description | replace: "\r\n", " " | replace: "\n", " " | strip_newlines | replace: "|", " " }} |
+| [{{ plugin.name }}]({{ plugin.url }}) | {{ plugin.description | replace: "\r\n", " " | replace: "\n", " " | strip_newlines | replace: "|", " " }} | {% if plugin.license -%} [{{ plugin.license.name }}]({{ plugin.license.url }}) {% else -%} n/a {% endif -%} |
 {% endfor -%}
-
-
 
